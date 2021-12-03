2df628dcbe554f69d9d8d1f68687dbb69cfaefab
Huib Piguillet <huib.piguillet@bynder.com>
2021-12-03 11:54:49 +0100

    Added ITypeToDictionaryConverter

--- Bynder/Test/Query/Decoder/QueryDecoderTest.cs
+++ Bynder/Test/Query/Decoder/QueryDecoderTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using Bynder.Sdk.Api.Converters;
 using Bynder.Sdk.Query.Decoder;
 using Xunit;
@@ -10,6 +11,23 @@ namespace Bynder.Test.Api
 {
     public class QueryDecoderTest
     {
+        private const string _stringItem1ApiField = "string1ApiField";
+        private const string _stringItem2ApiField = "string2ApiField";
+        private const string _dictItemApiField = "dictApiField";
+
+        private const string _item = "item";
+        private const string _stringItem1 = "string1";
+        private const string _stringItem2 = "string2";
+
+        private const string _converted = "converted";
+
+        private const string _dictKey1 = "dictKey1";
+        private const string _dictKey2 = "dictKey2";
+        private const string _dictKey3 = "dictKey3";
+        private const string _dictValue1 = "dictValue1";
+        private const string _dictValue2 = "dictValue2";
+        private const string _dictValue3 = "dictValue3";
+
         /// <summary>
         /// Tests that <see cref="QueryDecoder.GetParameters(object)"/> returns
         /// only the parameters properties that have the <see cref="ApiField"/> attribute.
@@ -20,16 +38,16 @@ namespace Bynder.Test.Api
             var queryDecoder = new QueryDecoder();
             var parameters = queryDecoder.GetParameters(new StubQuery
             {
-                Item1 = "1",
-                Item2 = "2",
-                Item3 = "3"
+                Item = _item,
+                StringItem1 = _stringItem1,
+                StringItem2 = _stringItem2
             });
 
-            // Property Item3 should not appear as it does not have APIField attribute
+            // Unannotated property should not appear as it does not have APIField attribute
             Assert.Equal(2, parameters.Count);
 
-            Assert.Equal("1", parameters["Item1"]);
-            Assert.Equal("2", parameters["Item2"]);
+            Assert.Equal(_stringItem1, parameters[_stringItem1ApiField]);
+            Assert.Equal(_stringItem2, parameters[_stringItem2ApiField]);
         }
 
         /// <summary>
@@ -42,10 +60,14 @@ namespace Bynder.Test.Api
             var queryDecoder = new QueryDecoder();
             var parameters = queryDecoder.GetParameters(new StubConverterQuery
             {
-                Item1 = "1"
+                StringItem = _stringItem1,
+                DictItem = 42
             });
 
-            Assert.Equal("Converted", parameters["Item1"]);
+            Assert.Equal(_converted, parameters[_stringItem1ApiField]);
+            Assert.Equal(_dictValue1, parameters[$"{_dictItemApiField}.{_dictKey1}"]);
+            Assert.Equal(_dictValue2, parameters[$"{_dictItemApiField}.{_dictKey2}"]);
+            Assert.Equal(_dictValue3, parameters[$"{_dictItemApiField}.{_dictKey3}"]);
         }
 
         /// <summary>
@@ -56,25 +78,25 @@ namespace Bynder.Test.Api
             /// <summary>
             /// Stub property.
             /// </summary>
-            [ApiField("Item1")]
-            public string Item1 { get; set; }
+            [ApiField(_stringItem1ApiField)]
+            public string StringItem1 { get; set; }
 
             /// <summary>
             /// Stub property.
             /// </summary>
-            [ApiField("Item2")]
-            public string Item2 { get; set; }
+            [ApiField(_stringItem2ApiField)]
+            public string StringItem2 { get; set; }
 
             /// <summary>
             /// Stub property.
             /// </summary>
-            public string Item3 { get; set; }
+            public string Item { get; set; }
         }
 
         /// <summary>
         /// Stub converter only used for testing purposes.
         /// </summary>
-        private class StubDecoder : ITypeToStringConverter
+        private class StubStringConverter : ITypeToStringConverter
         {
             /// <summary>
             /// Check <see cref="IParameterDecoder.CanConvert(Type)"/>.
@@ -93,7 +115,38 @@ namespace Bynder.Test.Api
             /// <returns>Check <see cref="ITypeToStringConverter.Convert(object)"/></returns>
             public string Convert(object value)
             {
-                return "Converted";
+                return _converted;
+            }
+        }
+
+        /// <summary>
+        /// Stub converter only used for testing purposes.
+        /// </summary>
+        private class StubDictionaryConverter : ITypeToDictionaryConverter
+        {
+            /// <summary>
+            /// Check <see cref="IParameterDecoder.CanConvert(Type)"/>.
+            /// </summary>
+            /// <param name="typeToConvert">Check <see cref="ITypeToStringConverter.CanConvert(Type)"/></param>
+            /// <returns>Check <see cref="ITypeToStringConverter.CanConvert(Type)"/></returns>
+            public bool CanConvert(Type typeToConvert)
+            {
+                return true;
+            }
+
+            /// <summary>
+            /// Check <see cref="IParameterDecoder.Convert(object)"/>.
+            /// </summary>
+            /// <param name="value">Check <see cref="ITypeToStringConverter.Convert(object)"/></param>
+            /// <returns>Check <see cref="ITypeToStringConverter.Convert(object)"/></returns>
+            public IDictionary<string, string> Convert(object value)
+            {
+                return new Dictionary<string, string>
+                {
+                    { _dictKey1, _dictValue1 },
+                    { _dictKey2, _dictValue2 },
+                    { _dictKey3, _dictValue3 }
+                };
             }
         }
 
@@ -105,8 +158,14 @@ namespace Bynder.Test.Api
             /// <summary>
             /// Stub property.
             /// </summary>
-            [ApiField("Item1", Converter = typeof(StubDecoder))]
-            public string Item1 { get; set; }
+            [ApiField(_stringItem1ApiField, Converter = typeof(StubStringConverter))]
+            public string StringItem { get; set; }
+
+            /// <summary>
+            /// Stub property.
+            /// </summary>
+            [ApiField(_dictItemApiField, Converter = typeof(StubDictionaryConverter))]
+            public int DictItem { get; set; }
         }
     }
 }
