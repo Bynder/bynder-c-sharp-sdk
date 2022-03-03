43f7f71172a104b39c30c16719f583c572514927
Reinhard Holzner <reinhard@smint.io>
2022-03-03 19:36:54 +0100

    Support media with count

---
+++ Bynder/Sdk/Model/CountValue.cs
@@ -0,0 +1,18 @@
+namespace Bynder.Sdk.Model
+{
+    /// <summary>
+    /// The count values as delivered by the API
+    /// </summary>
+    public class CountValue
+    {
+        /// <summary>
+        /// The property option key
+        /// </summary>
+        public string PropertyOptionKey { get; set; }
+
+        /// <summary>
+        /// the count
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
