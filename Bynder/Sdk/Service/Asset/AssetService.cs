662d100a933727528d5d0d8b8a81d2d46616e6cc
Quirijn Slings <quirijn.slings@trivident.com>
2024-08-23 14:59:10 +0200

    Make it possible to remove tags from an asset

--- Bynder/Sdk/Service/Asset/AssetService.cs
+++ Bynder/Sdk/Service/Asset/AssetService.cs
@@ -11,6 +11,7 @@ using Bynder.Sdk.Api.RequestSender;
 using Bynder.Sdk.Model;
 using Bynder.Sdk.Query.Asset;
 using Bynder.Sdk.Query.Upload;
+using System.Web;
 
 namespace Bynder.Sdk.Service.Asset
 {
@@ -204,6 +205,20 @@ namespace Bynder.Sdk.Service.Asset
                 Path = $"/api/v4/tags/{query.TagId}/media/",
                 HTTPMethod = HttpMethod.Post,
                 Query = query,
+            }).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Check <see cref="IAssetService"/> for more information
+        /// </summary>
+        /// <returns>Check <see cref="IAssetService"/> for more information</returns>
+        public async Task<Status> RemoveTagFromMediaAsync(string tagId, IEnumerable<string> assetIds)
+        {
+            var encodedIdList = HttpUtility.UrlEncode(string.Join(",", assetIds));
+            return await _requestSender.SendRequestAsync(new ApiRequest
+            {
+                Path = $"/api/v4/tags/{tagId}/media/?deleteIds={encodedIdList}",
+                HTTPMethod = HttpMethod.Delete,
             }).ConfigureAwait(false);
         }
 
